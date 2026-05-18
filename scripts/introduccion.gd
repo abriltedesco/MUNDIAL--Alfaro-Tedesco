d99aa1a94f34f7bdd3aec05adf91bb8eb1a44fb7
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2026-05-18 01:19:28 -0300

    modificando styles del globo del dialogo + dejando de usar example balloon
    duplicamos example balloon a la carpeta de dialogo para no usar el default.

--- scripts/introduccion.gd
+++ scripts/introduccion.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
+@onready var kiki = $kiki
+@onready var nani = $nani
 
 func _ready() -> void:
-	$kiki.movIntro = true
-	$nani.play("sleep")
-	$kiki.play("walkR")
+	kiki.movIntro = true
+	nani.play("sleep")
+	kiki.play("walkR")
 	
-	var tween = create_tween() # motor de animación por código
-	var posNani = Vector2($nani.position.x - 150, $nani.position.y)
-	tween.tween_property($kiki, "position", posNani, 3.0) # se mueve hacia donde está nani
+	var tween = create_tween()
+	var posNani = Vector2(nani.position.x - 150, nani.position.y)
+	tween.tween_property(kiki, "position", posNani, 3.0) 
 	await tween.finished
 	
-	$kiki.play("default")
+	kiki.play("default")
 	
-	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/dialogue.dialogue"), "start", [self])
+	var dialogo = load("res://dialogues/dialogue.dialogue")
+	var escenaGlobo = load("res://dialogues/globo_dialogo.tscn").instantiate()
+	add_child(escenaGlobo)
+	escenaGlobo.start(dialogo, "start", [self])

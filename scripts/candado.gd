dc28d6020db4c4525015cda62f90c8368d0c2fb5
abriltedesco <alumno26.tedesco.abril@ipm.edu.ar>
2026-05-19 15:52:07 -0300

    creando escena de niveles + modificando dialogos
    incluimos candado que controla el acceso a los niveles, mejoramos el cuadro de dialogo, agregamos la nueva escena del mapa con los niveles y se agrego el script de progeso para llevar registro del nivel q desbloqueamos

---
+++ scripts/candado.gd
@@ -0,0 +1,39 @@
+extends StaticBody2D
+
+@export var numNivel = 1
+var establoq = true
+
+func _ready() -> void:
+	$Label.mouse_filter = Control.MOUSE_FILTER_IGNORE #para q no bloquee los clics
+	input_pickable = true # permiso al candado de escuchar al mouse
+	input_event.connect(_on_input_event)
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	$Label.text = str(numNivel)
+		
+	if numNivel <= Progreso.nivelDesbloq:
+		$spriteCerrado.visible = false
+		$spriteAbierto.visible = true
+		establoq = false
+	else:
+		$spriteCerrado.visible = true
+		$spriteAbierto.visible = false
+		establoq = true
+		
+	
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if !establoq:
+			# para q no quede la manito flotando en el minijuego.
+			Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+			get_tree().change_scene_to_file("res://escenas/nivel_" + str(numNivel) + ".tscn")
+		else:
+			print("nivel bloqueado todavía")
+		
+
+func _on_mouse_entered() -> void:
+	if !establoq:
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	
+func _on_mouse_exited() -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
---
+++ scripts/progreso.gd
@@ -0,0 +1,10 @@
+extends Node
+var nivelDesbloq  =1
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

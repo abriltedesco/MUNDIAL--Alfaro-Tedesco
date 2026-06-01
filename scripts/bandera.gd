468353289aa695a4f7af466c9b2d37b42735e6ca
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2026-05-31 22:02:24 -0300

    agregando banderas a niveles
    (falta hacer a kiki o nani moviendose)

---
+++ scripts/bandera.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var escena= ""
+@export var banderaSprite = ""
+
+func _ready() -> void:
+	if banderaSprite != "":
+		$Sprite2D.texture = load(banderaSprite)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name != "kiki":
+		return
+	
+	if escena != "":
+		get_tree().change_scene_to_file(escena)

5e0f52f5047ba67ba9ac33679c0be422aa022b0f
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2026-06-04 09:47:24 -0300

    corrigiendo hover +  creando dialogo y escena de ganar

--- scripts/datoa_juego.gd
+++ scripts/datoa_juego.gd
@@ -3,6 +3,7 @@ class_name DatosJuego
 
 @export var huesitosRecolectado = 0
 @export var vioIntro= false 
+@export var gano=false
 @export var minijuegosGanados: Array[int] = []
 @export var huesosRecogidos: Array[int] = []
 @export var totalHuesitos = 4
---
+++ scripts/ganaste.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var kiki = $kiki
+@onready var nani = $nani
+
+func _ready() -> void:
+	kiki.movIntro = true
+	nani.play("sleep")
+	kiki.play("walkR")
+	
+	var tween = create_tween()
+	var posNani = Vector2(nani.position.x - 150, nani.position.y)
+	tween.tween_property(kiki, "position", posNani, 3.0) 
+	await tween.finished
+	
+	kiki.play("default")
+	
+	var dialogo = load("res://dialogues/dialogue.dialogue")
+
+	var escenaGlobo = load("res://dialogues/globo_dialogo.tscn").instantiate()
+	add_child(escenaGlobo)
+	escenaGlobo.start(dialogo, "ganaste", [self])
+	
+	await DialogueManager.dialogue_ended
+	
+	Progreso.datos.gano = true
+	Progreso.guardarPartida() 
+	
+	# get_tree().change_scene_to_file("res://escenas/nivelesDificiles.tscn")

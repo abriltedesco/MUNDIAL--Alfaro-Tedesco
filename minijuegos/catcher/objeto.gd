d4067cb7f3cd4d69c482f7888e0de572774a3315
torialfaro <ALUMNO26.ALFARO.Victoria@ipm.edu.ar>
2026-06-07 22:10:18 -0300

    randomizando mas objetos en el catcher

--- minijuegos/catcher/objeto.gd
+++ minijuegos/catcher/objeto.gd
@@ -2,19 +2,20 @@ extends Area2D
 
 var velocidadCaida:float=300.0
 @export var esBueno: bool=true
+var spritesBuenos: Array = ["croissant", "torta", "cafe"]
+var spritesMalos: Array  = ["malo1", "bueno"]
 
 func _ready() -> void:
-	$bueno.visible=esBueno
-	$malo.visible=!esBueno
-	
+	for nombre in spritesBuenos + spritesMalos:
+		get_node(nombre).visible = false
+	var lista: Array = spritesBuenos if esBueno else spritesMalos
+	get_node(lista[randi() % lista.size()]).visible = true
 	if Progreso.modoDificil:
 		velocidadCaida = 480.0
-		
 	if esBueno:
 		add_to_group("objetoBueno")
 	else:
 		add_to_group("objetoMalo")
-
 func _process(delta: float) -> void:
 	position.y+=velocidadCaida*delta
 	if position.y>get_viewport_rect().size.y+20:
